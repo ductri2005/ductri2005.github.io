73e0d0b53c80fc0ef8552c8e48b71c4f0b5bc283
ductri2005 <117645133+ductri2005@users.noreply.github.com>
2022-11-07 18:59:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu emm!",
+  text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Em đã yêu anh chưa ._.",
+  text4: "Không trả lời thật lòng bùn đó em =)))",
+  text5: "em yêu anh mà<3",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "mà tại sao em đồng ý iêu anhh",
+  text8: "gửi cho anhh",
+  text9: "Vì anh đẹp trai vclll",
+  text10: "Anh biết mà ^^ Yêu em 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Cùng cố gắng học nhé",
   text12: "Okii lunn <3",
 };
 
